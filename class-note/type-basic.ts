36acfa4d9f494ab27d4589fd56a642aed9d8f2a3
yunseo323 <jokani3244833@gmail.com>
2021-08-20 10:44:50 +0900

    data

---
+++ class-note/type-basic.ts
@@ -0,0 +1,24 @@
+//문자열 
+let str: string = "hello"; 
+
+//숫자 
+let num: number = 10; 
+
+//배열
+let items: number[]=[1,2,3];
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ["capt","thor","hulk"];
+
+//튜플
+let address: [string,number]=["hi",10];
+
+//객체
+let obj: object={};
+
+let person:{name:string, age:number} = {
+    name:"yun",
+    age: 20
+}
+
+//진위값
+let show: boolean = true;
