6968dc2901e2498fa6e71f9051fefc46b725c4ae
yunseo323 <jokani3244833@gmail.com>
2021-08-21 21:21:16 +0900

    4_type aliases

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,30 @@
+
+/*
+interface Person {
+    name: string;
+    age: number;
+}
+*/
+
+type Person_ = {
+    name: string;
+    age: number;
+}
+
+let yunseo: Person_ = {
+    name: 'yunseo',
+    age: 30
+}
+
+type MyString = string;
+let str: MyString = "hello";
+
+type Todo = {
+    id: string;
+    title: string;
+    dobe: boolean;
+}
+
+function getTodo(todo:Todo){
+    
+}
