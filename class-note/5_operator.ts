10c2eac7ce4aa79af7f97c42ec68231eda053be9
yunseo323 <jokani3244833@gmail.com>
2021-08-22 14:33:28 +0900

    add intersection type

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -11,7 +11,7 @@ function logMessage(value: string | number){
 logMessage("hello");
 logMessage(10); 
 
-interface Developer {
+interface Developer_ {
     name: string;
     skill: string;
 }
@@ -21,11 +21,24 @@ interface Person {
     age: number;
 }
 
-function askSomeone(someone:Developer|Person) {
+function askSomeone(someone:Developer_|Person) {
     someone.name; //공통된 property 에 대해서만 제공함
     /*
     접근 x
     someone.skill
     someone.age
     */
-})
+}
+
+function ask_Someone(someone:Developer_&Person) {
+    // & 인터섹션 타입을 사용하면 공통이지 않은 property에도 접근 가능하다
+    someone.name; 
+    someone.skill;
+    someone.age; 
+}
+
+// union 활용
+askSomeone({name:"yun", skill:"ts"}); //Developer_ 타입 선택
+askSomeone({name: "yun", age: 22}) //Persion 타입 선택
+//intersection 활용
+ask_Someone({name:"yun", skill:"ts", age: 22}) // 두 타입을 합친, 양쪽의 property를 다 넘겨야 함
