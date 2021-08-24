f00ccc4b10402fcae83e999ddeb543db1d1df0bf
yunseo323 <jokani3244833@gmail.com>
2021-08-25 08:31:01 +0900

    generics

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -41,6 +41,8 @@ const str = logText<string>('abc');
 str.split(''); //string api 사용 가능
 const login = logText<boolean>(true); //boolean 변경 가능
 
+////////////////////
+
 //인터페이스에 제네릭을 선언하는 방법
 
 interface Dropdown<T>{
@@ -50,3 +52,43 @@ interface Dropdown<T>{
 
 const obj: Dropdown<number> = {value: 10, selected: true};
 const obj_: Dropdown<string> = {value:"hello",selected: false};
+
+// 제네릭의 타입 제한
+function logTextLength<T>(text: T[]): T[]{
+    console.log(text.length);
+    
+    return text;
+} 
+//logTextLength<string>("hi")
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+
+interface LengthType {
+    length: number;
+}
+
+function logTextLength_<T extends LengthType>(text: T): T{
+    text.length;
+    return text;
+}
+
+logTextLength_("hello");
+
+//제네릭 타입 제한 3 - keyof
+
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+//interface ShoppingItem 안의 key값들만 받을 수 있도록 제약 가능
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T):T {
+    return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+
+getShoppingItemOption("name");
--- class-note/generic_code/dropdown-generic.ts
+++ class-note/generic_code/dropdown-generic.ts
@@ -1,26 +1,40 @@
-interface Email {
-  value: string;
+interface DropdownItem<dr>{
+  value: dr;
   selected: boolean;
 }
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
 
-const emails: Email[] = [ //type
+const emails: DropdownItem<string>[] = [ //type
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
 
-const numberOfProducts: ProductNumber[] = [ //type
+const numberOfProducts: DropdownItem<number>[] = [ //type
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email|ProductNumber) { //type
+/*
+function createDropdownItem(item: DropdownItem<string>|DropdownItem<number>) { //type
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+*/
+
+function createDropdownItem_<T>(item: DropdownItem<T>){ //function에 제네릭 추가 - union 없앨수 있다!
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -30,13 +44,13 @@ function createDropdownItem(item: Email|ProductNumber) { //type
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem_<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
 numberOfProducts.forEach(function(product){
-  const item = createDropdownItem(product);
+  const item = createDropdownItem_<number>(product);
 })
 /*
 1. 인터페이스를 달아, 유니온을 사용해 이용함 
